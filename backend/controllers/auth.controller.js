78d0f5e475cc2b53f4043cdda471e8fe91ed4eef
dipayan debnath <debnathdipayan042@gmail.com>
2024-03-28 02:45:44 +0530

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
@@ -12,18 +12,26 @@ export const signup = async (req, res) => {
     if (user) {
       return res.status(400).json({ msg: "User already exists" });
     }
+    
 
+    //hashed password
+
+const salt =await bcrypt.genSalt(10);
+const hashPassword= await bcrypt.hash(password,salt);
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const gitlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : gitlProfilePic,
     });
 
+   if(newUser){
+    //Generate JWT token here
+    
     await newUser.save();
 
     res.status(201).json({
@@ -31,6 +39,10 @@ export const signup = async (req, res) => {
       username: newUser.username,
       profilePic: newUser.profilePic,
     });
+   }
+   else{
+    res.status(400).json({error:"invalid user data"});
+   }
   } catch (error) {
     console.log("Error  in SignUp", error.message);
     res.status(500).json({ error: "Internal server Error" });
