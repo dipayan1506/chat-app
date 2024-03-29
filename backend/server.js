773ebe728c93393d6b5b3d884e50930e35321f22
dipayan debnath <debnathdipayan042@gmail.com>
2024-03-30 02:22:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
+
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from  "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
@@ -15,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
 
