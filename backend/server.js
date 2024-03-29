74f459a1a5b76b52d9805e36ee430b9cf9ac7ec9
dipayan debnath <debnathdipayan042@gmail.com>
2024-03-30 01:57:35 +0530

    middleware added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from  "./routes/message.routes.js";
+import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 config();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
